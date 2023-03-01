5fe726301c9ab0f551cef22799d266c01a0659d9
Callum Goss <cgoss3@my.bcit.ca>
2023-03-01 11:39:22 -0800

    Added firebase login

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
 function sayHello() {
-    
+    console.log("Loaded");
 }
-//sayHello();
+sayHello();
---
+++ scripts/skeleton.js
@@ -0,0 +1,4 @@
+function loadSkeleton(){
+    console.log($('#footerPlaceholder').load('./text/footer.html'));
+}
+loadSkeleton();
